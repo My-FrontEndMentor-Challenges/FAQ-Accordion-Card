683519532e22659d4698babe6de3c6a4b6c8dc4e
Victor Martinez Jr <victormtzcodes@gmail.com>
2022-03-29 16:43:23 -0400

    refactored javaScript

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 "use strict";
 
-const question = document.querySelector(".single-question");
+const questionNode = document.querySelectorAll(".single-question");
 
-question.addEventListener("click", (e) => {
-  const answer = document.querySelector(".answer");
-  const questionText = document.querySelector(".single-question p");
-  const arrowIcon = document.querySelector(".arrow-icon");
-  questionText.classList.toggle("selected-question");
-  arrowIcon.classList.toggle("arrow-icon-active");
-  answer.classList.toggle("show-answer");
+// Turn nodeList into Array
+const questions = [...questionNode];
+
+questions.forEach((question, i) => {
+  question.addEventListener("click", (e) => {
+    const clickedItem = e.target.closest("div");
+    const answer = clickedItem.querySelector(".answer");
+    const questionText = clickedItem.querySelector(".question-text");
+    const arrowIcon = clickedItem.querySelector(".arrow-icon");
+    questionText.classList.toggle("selected-question");
+    arrowIcon.classList.toggle("arrow-icon-active");
+    answer.classList.toggle("show-answer");
+  });
 });
